f1b1c4a8309b757bc5e05cabb8c0ad262051a835
Mark5213 <marcuse0@hotmail.com>
2018-03-01 16:14:13 +0100

    storeadmin

--- src/store/sim/StoreAdmin.java
+++ src/store/sim/StoreAdmin.java
@@ -1,5 +1,12 @@
 package store.sim;
 
 public class StoreAdmin {
-
+private int register = 4;
+private int maxcustomer = 25;
+int registers() {
+	return register;
+}
+int highestcustomer(){
+	return maxcustomer;
+}
 }
