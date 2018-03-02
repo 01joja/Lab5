4229d4e91561571a19042a471df210542267bfdb
Mark5213 <marcuse0@hotmail.com>
2018-03-02 15:33:05 +0100

    stoewview

--- src/store/sim/StoreView.java
+++ src/store/sim/StoreView.java
@@ -1,16 +1,29 @@
 package store.sim;
 
+import java.util.Observable;
 import deds.SimView;
-
+import store.sim.StoreAdmin;
 public class StoreView extends SimView {
-       private  StoreState theState;
-       
-       public StoreView(StoreState state) {
-    	   stateOfStore(theState);
+       private  StoreState theStateStore;
+       private StoreAdmin theStateAdmin;
+       public StoreView(StoreState theStateStore) {
+    	   stateOfStore(theStateStore);
        }
 
-	public void stateOfStore(StoreState state) {
-		this.theState = theState;
+	public void stateOfStore(StoreState theStateStore) {
+		this.theStateStore = theStateStore;
 	}
-	
+    public void StoreTheAdmin(StoreAdmin theStateAdmin) {
+    	StoreTheAdmin(theStateAdmin);
+    }
+
+
+	public void stateOfStoreAdmin(StoreAdmin theStateAdmin) {
+		this.theStateAdmin = theStateAdmin;
+	}
+	public void update(Observable o, Object arg) {
+		System.out.println(String.format("Registerss: ",theStateAdmin.registers()));
+		System.out.println(String.format("Max costumers: %s",	theStateAdmin.maxCustomers()));
+		System.out.println(String.format("Max costumers: %s", theStateStore.addCustomer()));
+	}
 }
