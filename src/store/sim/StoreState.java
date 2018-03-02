fc859a60cee0516c3e4d6b1af1d3d97ab262ce43
Klomega <klomega89@gmail.com>
2018-03-02 14:05:08 +0100

    mer saker

--- src/store/sim/StoreState.java
+++ src/store/sim/StoreState.java
@@ -6,7 +6,7 @@ public class StoreState extends SimState {
 	
 	private double queuedTime = 0;
 	private int paid = 0;
-	private int angry = 0;
+	private static int angry = 0;
 	private int queued = 0;
 	private int currentlyQueuing = 0;
 	private int registerQueue = 0;
@@ -15,6 +15,18 @@ public class StoreState extends SimState {
 	
 	public static int customersInStore() {
 		return customersInStore;
+	}
+	
+	public void addCustomer() {
+		customersInStore += 1;
+	}
+	
+	public void removeCustomer() {
+		customersInStore -= 1;
+	}
+	
+	public static void angry() {
+		angry += 1;
 	}
 
 }
