3eb61092382df8f018674504082e7a89176d723f
01joja <01joja@gmail.com>
2018-03-05 16:15:12 +0100

    open

--- src/store/events/Open.java
+++ src/store/events/Open.java
@@ -3,5 +3,10 @@ package store.events;
 import deds.EventStart;
 
 public class Open extends EventStart {
-
+	
+	public Open(double startTime, double finishTime) {
+		super(startTime, finishTime);
+		
+	}
 }
+
