90a16a0443dc12e0cbf6fe7615ad396c44e16f12
Klomega <klomega89@gmail.com>
2018-03-05 13:38:51 +0100

    event

--- src/deds/Event.java
+++ src/deds/Event.java
@@ -5,11 +5,7 @@ public abstract class Event {
 	private double startTime;
 	private double finishTime;
 	private double time;
-	private int SIMSTART = 0;
-	private int ARRIVES = 1;
-	private int PICKGOODS = 2;
-	private int PAYING = 3;
-	private int SIMSTOP = 4;
+	
 	
 	public void perform() {
 		
