e9902541373adf16e4d9ec4ec82c5989802fd918
Klomega <klomega89@gmail.com>
2018-03-05 09:52:04 +0100

    jjjk

--- src/store/sim/StoreState.java
+++ src/store/sim/StoreState.java
@@ -6,13 +6,16 @@ import deds.SimState;
 public class StoreState extends SimState {
 	
 	private double queuedTime = 0;
+	private double timeRegistersNotUsed = 0;
 	private int paid = 0;
 	private int angry = 0;
 	private int queued = 0;
 	private int currentlyQueuing = 0;
 	private int registerQueue = 0;
-	private int registersUsed = 0;
+	private int emptyRegisters = 0;
 	private int customersInStore = 0;
+	private int customersQueued = 0;
+	private int customersInLine[];
 	private int OPEN = 0;
 	private int CLOSED = 1;
 	
