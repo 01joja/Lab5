6fca2779d9efafe75d95c3d85133980751498e2b
Mark5213 <marcuse0@hotmail.com>
2018-03-05 15:30:32 +0100

    view

--- src/store/sim/StoreView.java
+++ src/store/sim/StoreView.java
@@ -9,6 +9,17 @@ public class StoreView extends SimView {
        private StoreAdmin theStateAdmin;
        public StoreView(StoreState theStateStore) {
     	   stateOfStore(theStateStore);
+   		System.out.println("PARAMETRAR");
+   		System.out.println("==========");
+   		System.out.println(String.format("Antal kassor, N..........: ",theStateAdmin.registers()));
+   		System.out.println(String.format("Max som ryms, M..........: ",	theStateAdmin.maxCustomers()));
+   		System.out.println(String.format("Plocktider, [P_min..Pmax]:", theStateStore.customersInStore() ));
+   		System.out.println(String.format("Betaltider, [K_min..Kmax]:", theStateStore.customersInStore() ));
+   		System.out.println(String.format("FrÃ¶, f...................:", theStateStore.customersInStore() ));
+   		System.out.println("FÃ–RLOPP");
+   		System.out.println(("=======");
+   		System.out.println(("Tid HÃ¤ndelse  Kund  ?  led    ledT    I     $    :-(   kÃ¶at    kÃ¶T   kÃ¶ar  [KassakÃ¶..]");
+   		System.out.println(String.format("costumers in store:", theStateStore.queuedTime(null) ));
        }
 
 	public void stateOfStore(StoreState theStateStore) {
@@ -23,17 +34,7 @@ public class StoreView extends SimView {
 		this.theStateAdmin = theStateAdmin;
 	}
 	public void update(Observable o, Object arg) {
-		System.out.println("PARAMETRAR");
-		System.out.println("==========");
-		System.out.println(String.format("Antal kassor, N..........: ",theStateAdmin.registers()));
-		System.out.println(String.format("Max som ryms, M..........: ",	theStateAdmin.maxCustomers()));
-		System.out.println(String.format("Plocktider, [P_min..Pmax]:", theStateStore.customersInStore() ));
-		System.out.println(String.format("Betaltider, [K_min..Kmax]:", theStateStore.customersInStore() ));
-		System.out.println(String.format("FrÃ¶, f...................:", theStateStore.customersInStore() ));
-		System.out.println("FÃ–RLOPP");
-		System.out.println(("=======");
-		System.out.println(("Tid HÃ¤ndelse  Kund  ?  led    ledT    I     $    :-(   kÃ¶at    kÃ¶T   kÃ¶ar  [KassakÃ¶..]");
-		System.out.println(String.format("costumers in store:", theStateStore.queuedTime(null) ));
+
 		
 	}
 }
