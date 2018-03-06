18fff32bb96b478b7425ece967d68ddb8971f913
01joja <01joja@gmail.com>
2018-03-06 15:53:11 +0100

    test

--- src/store/sim/StoreState.java
+++ src/store/sim/StoreState.java
@@ -7,6 +7,12 @@ import store.sim.*;
 
 public class StoreState extends SimState {
 	
+	private final double LAMBDA = 4;
+	private final double P_MIN = 0.6;
+	private final double P_MAX = 0.9;
+	private final double K_MIN = 0.35;
+	private final double K_MAX = 0.6;
+	
 	private final int MAXCOSTUMER;
 	private final int REGISTERS;
 	private double queuedTime = 0;
@@ -19,8 +25,8 @@ public class StoreState extends SimState {
 	private int customersInStore = 0;
 	private int customersQueued = 0;
 	private boolean storeIsOpen = false;
+	
 	private FIFO fifo;
-	private double lambda = 4;
 	private ExponentialRandomStream arivalRandom;
 	
 	public StoreState(int maxCustomers, int registers, double timeStoreIsOpen) { 
