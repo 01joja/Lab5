c280a794d118aec5291474fc8ce7d7abc6a564d8
Mark5213 <marcuse0@hotmail.com>
2018-03-06 17:09:03 +0100

    goods

--- src/store/events/PickGoods.java
+++ src/store/events/PickGoods.java
@@ -3,5 +3,21 @@ package store.events;
 import deds.Event;
 
 public class PickGoods extends Event {
+	public class PickGoods extends Event {	
+		private Event PickGoods;
+		public  PickGoods() {
+		setTime;
+		super.setNameOfEvent("PickGoods");
+		Customer customer = new Customer();
+		EventQueue.getEventQueue(PickGoods);
+	}
+
+	public void perform (StoreState s) {
+		s.update;
+		s.isStoreFull();
+		Pay pay = new Pay();
+		PickGoods pickgoods = new PickGoods();
+		}
+	}
 
 }
