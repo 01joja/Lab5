4575577f467335d5e65af05820e358a7e64ac93d
01joja <01joja@gmail.com>
2018-03-06 16:07:30 +0100

    added getMasCostumers and getRegisters

--- src/store/sim/StoreState.java
+++ src/store/sim/StoreState.java
@@ -25,8 +25,10 @@ public class StoreState extends SimState {
 	private int customersInStore = 0;
 	private int customersQueued = 0;
 	private boolean storeIsOpen = false;
+	
 	private FIFO fifo;
 	private ExponentialRandomStream arivalRandom;
+	private 
 	
 	
 
@@ -35,7 +37,7 @@ public class StoreState extends SimState {
 		this.REGISTERS = registers;
 		this.MAXCOSTUMER = maxCustomers;
 		arivalRandom = new ExponentialRandomStream(LAMBDA);
-		new Arivals();
+		new Arrivals();
 	}
 	
 	public StoreState(int maxCustomers, int registers, double timeStoreIsOpen, long seed) { 
@@ -47,6 +49,14 @@ public class StoreState extends SimState {
 
 	public int customersInStore() {
 		return this.customersInStore;
+	}
+	
+	public int getMaxCustomers(){
+		return this.MAXCOSTUMER;
+	}
+	
+	public int getRegisters(){
+		return this.REGISTERS;
 	}
 		
 	public double queuedTime() {
