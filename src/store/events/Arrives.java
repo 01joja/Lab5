314569e76ef91df33abfcedfeece1b1bd17a14fd
Klomega <klomega89@gmail.com>
2018-03-06 15:28:48 +0100

    jkjJiljkuhgku

--- src/store/events/Arrives.java
+++ src/store/events/Arrives.java
@@ -1,7 +1,9 @@
 package store.events;
 
 import deds.Event;
+import deds.EventQueue;
 import deds.SimState;
+import store.sim.Customer;
 import store.sim.StoreAdmin;
 import store.sim.StoreState;
 
@@ -11,10 +13,17 @@ public class Arrives extends Event{
 	private double startTime;
 	private double finishTime;
 		
-			
-	public void perform (StoreState s, StoreAdmin a) {
+	Arrives(EventQueue e) {
+		private setTime;
+		event = arrives;
+		Customer customer = new Customer();
+		private Arrives.addEvent;
+	}
+	
+	public void perform (StoreState s, StoreAdmin a, Customer c) {
+		Customer customer = new Customer();
+		customer.isStoreFull();
 		
-			
 		}
 		
 		
