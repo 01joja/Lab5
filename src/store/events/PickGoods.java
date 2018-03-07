f33971b119a5e9e4dfff2ef24972a4398642340d
01joja <01joja@gmail.com>
2018-03-07 11:15:24 +0100

    hej

--- src/store/events/PickGoods.java
+++ src/store/events/PickGoods.java
@@ -2,42 +2,27 @@ package store.events;
 
 import deds.Event;
 import deds.EventQueue;
-import deds.SimState;
-import store.sim.Customer;
-import store.sim.StoreState;
+import store.sim.*;
 
 public class PickGoods extends Event {
 	private EventQueue eventQueue;
-	private StoreState s;
+	private StoreState storeState;
 	private Customer customer;
+	private UniformRandomStream randomTime;
 		
 	PickGoods(Customer customer, StoreState storeState) {
-		//setTime;
-		// 
-		this.s=s;
-		//Tar emot the föregående eventet (Arrivals)
-		this.eventQueue = s.getEventQueue();
-		//ändrar namnet på det nuvarande eventet
-		super.setNameOfEvent("PickGoods");
-<<<<<<< HEAD
-		//Sätter ID på kunden som går in i PickGoods eventet
-		this.customer = new Customer();
-		//Adderar eventet till classen EventQueue
-		this.eventQueue.addEvent(this);;
-	}
-
-	public void perform () {
-		//uppdaterar StoreState
-=======
+		this.storeState = storeState;
+		this.customer = customer;
+		this.eventQueue = this.storeState.getEventQueue();
+		this.randomTime = this.storeState.getPickGoodsRandom();
+		setTime(this.randomTime.next() + this.storeState.getTime());
+		setNameOfEvent("PickGoods");
 		this.eventQueue.addEvent(this);
 	}
 
 	public void perform () {
-		s.updateStore(this, customer);
-		Pay pay = new Pay(customer, s);
->>>>>>> 372dd9e38da518e16799944b540e7969993d6c66
-		s.updateStore(this,customer);
-//		Pay pay = new Pay();
-		PickGoods pickgoods = new PickGoods(s);
+		storeState.updateStore(this, customer);
+		storeState.setTime(getEventFinishTime());
+//		new Pay(customer, this.storeState);
 		}
 	}
