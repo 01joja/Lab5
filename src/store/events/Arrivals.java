ed183a0aa2bb03f932360529456a0cbd80842498
01joja <01joja@gmail.com>
2018-03-07 11:35:39 +0100

    events

--- src/store/events/Arrivals.java
+++ src/store/events/Arrivals.java
@@ -17,7 +17,7 @@ public class Arrivals extends Event{
 	private EventQueue eventQueue;
 	
 	//När ett nytt Arrivalobjekt skapas så får den en tid, läggs till i EvenQueue, döps till Arrival och lägger till en ny kund.
-	public Arrivals(StoreState s, ExponentialRandomStream getTime, double time) {
+	public Arrivals(StoreState s, ExponentialRandomStream getTime) {
 		this.storeState=s;
 		this.exponentialRandomStream = getTime;
 		this.setTime(this.exponentialRandomStream.next() + storeState.getTime());
