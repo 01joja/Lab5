b8dd5856243f578f87a4b611664c54dad4082e5f
Mark5213 <marcuse0@hotmail.com>
2018-03-07 13:34:08 +0100

    a

--- src/store/events/Open.java
+++ src/store/events/Open.java
@@ -2,20 +2,15 @@ package store.events;
 
 import deds.EventStart;
 import deds.SimState;
-
+/**
+ * Innehåller en konstruktor som öppnar butiken
+ * Ger en öppnings tid, en stängings tid
+ * och själva simulatorns stängningstid
+ *
+ */
 public class Open extends EventStart {
-	
-	private double closeTime;
-	
-	public Open(double openTime, double closeTime, double simStopTime, SimState simState){
+	public Open(double OpenTime, double CloseTime, double simStopTime, SimState simState){
 		super(simStopTime, simState);
-		this.setTime(openTime);
-		this.setNameOfEvent("Open");
-		this.closeTime = closeTime;
-	}
-	
-	void preform(){
-		
 	}
 }
 
