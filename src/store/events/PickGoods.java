16eac19b0e669258c0d1e4997bee1e5db1b173b1
Mark5213 <marcuse0@hotmail.com>
2018-03-07 10:10:49 +0100

    aaa

--- src/store/events/PickGoods.java
+++ src/store/events/PickGoods.java
@@ -14,14 +14,20 @@ import store.sim.StoreState;
 		
 		public  PickGoods(StoreState s) {
 		//setTime;
+		// 
 		this.s=s;
+		//Tar emot the föregående eventet (Arrivals)
 		this.eventQueue = s.getEventQueue();
+		//ändrar namnet på det nuvarande eventet
 		super.setNameOfEvent("PickGoods");
+		//Sätter ID på kunden som går in i PickGoods eventet
 		this.customer = new Customer();
+		//Adderar eventet till classen EventQueue
 		this.eventQueue.addEvent(this);;
 	}
 
 	public void perform () {
+		//uppdaterar StoreState
 		s.updateStore(this,customer);
 //		Pay pay = new Pay();
 		PickGoods pickgoods = new PickGoods(s);
