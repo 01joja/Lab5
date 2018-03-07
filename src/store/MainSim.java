395ae6e82513c7ac1304720cf03f0b522c62ffc8
Mark5213 <marcuse0@hotmail.com>
2018-03-07 10:47:30 +0100

    add

--- src/store/MainSim.java
+++ src/store/MainSim.java
@@ -1,11 +1,24 @@
 package store;
 import deds.Simulator;
 import store.sim.StoreState;
-
+/**
+ * 
+ * @author's 
+ * Jonas Jacobsson 
+ * Marcus Carlsson 
+ * Tommy Andersson 
+ * Marcus Erisson amueri-6
+ */
 public class MainSim {
-
+    /**
+     * 
+     * @param args
+     */
 	public static void main(String[] args) {
+		//Ger Storestate 4 parametrar med värden för Antalet kunder som ryms
+		// Antalet kassor,
 		StoreState storeState = new StoreState(2, 5, 10.0, 1234);
+		
 		Simulator StorSimulator = new Simulator(storeState);
 		StorSimulator.mainLoop();
 	}
